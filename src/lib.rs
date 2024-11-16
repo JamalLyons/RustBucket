c90ea5d44e51235a8c301efd16c8fd650b8613ae
CodingWithJamal <43446907+jamallyons@users.noreply.github.com>
2024-11-16 17:23:00 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,6 @@ pub mod vm;
 
 // Re-export commonly used items
 pub use assembler::Assembler;
-pub use vm::{VMConfig, VMError, CPU};
+pub use vm::cpu::CPU;
+pub use vm::error::VMError;
+pub use vm::VMConfig;
